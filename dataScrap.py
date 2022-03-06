5dbe403ebdf375142f46ce96154db14a7a2566cd
Harkirat12345 <96738813+Harkirat12345@users.noreply.github.com>
2022-03-06 10:03:34 -0500

    Add files via upload

---
+++ dataScrap.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import csv
+START_URL = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+page = requests.get(START_url)
+print(page)
+soup = bs(page.text,'html.parser')
+tar_table = soup.find('table')
+
+temp_list= []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+    Star_names = []
+name = []
+distance = []
+mass = []
+radius = []
+
+for i in range(1, len(temp_list)):
+    name.append(temp_list[i][1])
+    distance.append(temp_list[i][3])
+    mass.append(temp_list[i][5])
+    radius.append(temp_list[i][6])
+
+df = pd.DataFrame(
+    list(zip(name, distance, mass, radius)),
+    columns=["Star_name", "Distance", "Mass", "Radius"],
+)
+df.to_csv("data.csv")
+
+
+
+
